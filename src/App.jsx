5dca85e89bcc40b88848b0ace97a0504a36ca436
Linda BOUGAA <lindabgaa@Host-001.lan>
2024-09-19 15:28:07 +0200

    Create HomePage component with styles and set up routing
    - Implemented HomePage component and its associated styles
    - Configured routes in App component for HomePage and other game pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HangmanPage from "./pages/HangmanPage/HangmanPage";
+import Home from "./pages/HomePage/HomePage";
+import MastermindPage from "./pages/MastermindPage/MastermindPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import TicTacToePage from "./pages/TicTacToePage/TicTacToePage";
+
+import "./App.css";
+
 export default function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Hangman" element={<HangmanPage />} />
+        <Route path="/Mastermind" element={<MastermindPage />} />
+        <Route path="/TicTacToe" element={<TicTacToePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,1 +1,48 @@
+import { Link } from "react-router-dom";
 
+import styles from "./HomePage.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.mainContainer}>
+      <p>Choose a game and let the fun begin!</p>
+      <div className={styles.loadingDots}>
+        <div className={styles.dot}>.</div>
+        <div className={styles.dot}>.</div>
+        <div className={styles.dot}>.</div>
+      </div>
+      <nav>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link
+              to="/Hangman"
+              className={styles.link}
+              aria-label="Play Hangman game"
+            >
+              Hangman
+            </Link>
+          </li>
+
+          <li className={styles.navItem}>
+            <Link
+              to="/Mastermind"
+              className={styles.link}
+              aria-label="Play Mastermind game"
+            >
+              Mastermind
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link
+              to="/TicTacToe"
+              className={styles.link}
+              aria-label="Play TicTacToe game"
+            >
+              TicTacToe
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
